816e695911263c6120699c5b7161f5fd98cab77c
unknown <vijay.ror@gmail.com>
2020-02-24 10:17:09 +0530

    added Access-Control-Allow

--- src/server.js
+++ src/server.js
@@ -4,11 +4,19 @@ import affiliate from './affiliate'
 
 const app = express()
 
+
 app.use('/women', affiliate)
 
 app.get('/', (req, res) => {
     
     res.send('some message')
 })
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.listen(3001, () => console.log('server start with listen port: 3001'))
